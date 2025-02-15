6ec72adc6cfe669708a7bb1c917ab3c5c7b9466e
root <root@ip-172-31-45-245.ap-south-1.compute.internal>
2025-02-15 14:43:08 +0000

    hdfhed

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-templates = Jinja2Templates(directory="/code")  # Make sure this is the correct directory path
+templates = Jinja2Templates(directory="/opt")  # Make sure this is the correct directory path
 
 @app.get("/")  # Corrected the decorator
 def form_port(request: Request):
