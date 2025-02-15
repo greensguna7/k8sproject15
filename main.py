208e9850a8bd641b3f46a90d9d08a14517662804
root <root@ip-172-31-45-245.ap-south-1.compute.internal>
2025-02-15 14:20:48 +0000

    hghdg

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-#uvicorn main: app --reload
-from fastapi import FastAPI,Request,Form
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-templates = Jinja2Templates(directory = "/opt">
-#templates = Jinja2Templates(directory = "D:/balan/DevOps/kubernetes">
+templates = Jinja2Templates(directory="/opt")  # Make sure this is the correct directory path
 
-@app-get ("/")
-def form_port (request: Request): return templates.TemplateResponse ('form.html', context = {{'request': request})
- 
+@app.get("/")  # Corrected the decorator
+def form_port(request: Request):
+    return templates.TemplateResponse("form.html", context={"request": request})  # Corrected the context
 
